818ea6b24f14dd17eb5e4d9d272e7b9284abb1ad
clmatherson <70039543+clmatherson@users.noreply.github.com>
2020-12-20 22:32:13 -0500

    Add files to Git Hub

---
+++ static/js/app.js
@@ -0,0 +1,106 @@
+function optionChanged(id) {
+    buildMetaData(id);
+    buildBarChart(id);
+    buildBubbleChart(id);
+    buildGaugeChart(id);
+};
+
+function buildBarChart(id) {
+    d3.json("../samples.json").then((sample) => {
+        var sel_sampleData = sample.samples;
+        var result = sel_sampleData.filter((smdata) => smdata.id.toString() === id)[0];
+
+        var sampleValues =  result.sample_values.slice(0, 10).reverse();
+
+        var otu = (result.otu_ids.slice(0, 10)).reverse();
+        var otu_id = otu.map((id) => `OTU  ${id}`);
+
+        var labels =  result.otu_labels.slice(0, 10).reverse();
+
+        var trace = {
+            x: sampleValues,
+            y: otu_id,
+            text: labels,
+            marker: {
+                color: 'blue'
+            },
+            type: "bar",
+            orientation: "h",
+        };
+        var data = [trace];
+
+        var layout = {
+            title: `${id} Top ${otu_id.length} OTU`,
+            yaxis:{
+                tickmode: "linear",
+            }
+        };
+        Plotly.newPlot("bar", data, layout);
+    });
+};
+
+
+function buildBubbleChart(id) {
+    d3.json("../samples.json").then((sample) => {
+        var sel_sampleData = sample.samples
+        var result = sel_sampleData.filter((smdata) => smdata.id.toString() === id)[0];
+
+        var sampleValues =  result.sample_values.slice(0, 10).reverse();
+
+        var otu = (result.otu_ids.slice(0, 10)).reverse();
+        var otu_id = otu.map((id) => `OTU  ${id}`);
+        var otu_ids = otu.map((id) => id);
+
+        var labels =  result.otu_labels.slice(0, 10).reverse();
+
+        var trace = {
+            x: otu_id,
+            y: sampleValues,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otu_ids
+            },
+            text: labels
+        };
+        var data = [trace];
+
+        var layout = {
+            xaxis: {title: `OTU ${id}`},
+            height: 600,
+            width: 1000,
+            };
+        Plotly.newPlot("bubble", data, layout);
+    });
+};
+
+function buildMetaData(id) {
+    d3.json("../samples.json").then((sample) => {
+        var sel_metaData = sample.metadata;
+
+        var result = sel_metaData.filter((smdata) => smdata.id.toString() === id)[0];
+        var sel_metaData_id = d3.select("#sample-metadata");
+        
+        sel_metaData_id.html("");
+
+        Object.entries(result).forEach((item) => {
+            sel_metaData_id.append("h5").text(item[0].toUpperCase() + ":" + item[1] + "\n");
+        });    
+    });
+};
+
+function init() {
+    var dropdown = d3.select("#selDataset");
+    
+    d3.json("../samples.json").then((selectedData) => {
+        
+        selectedData.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+        buildMetaData(selectedData.names[0]);
+        buildBarChart(selectedData.names[0]);
+        buildBubbleChart(selectedData.names[0]);
+    });
+};
+
+init();
---
+++ static/js/bonus.js
@@ -0,0 +1,61 @@
+function buildGaugeChart(id) {
+  d3.json("../samples.json").then((sample) => {
+    var sel_metaData = sample.metadata;
+    var result = sel_metaData.filter((smdata) => smdata.id.toString() === id)[0];
+    var data = [
+        {
+          type: "indicator",
+          mode: "gauge+number+delta",
+          value: Math.max(parseInt(result.wfreq)),
+          title: { text: "Belly Button Washing Frequency <br> Scrubs per Week", font: { size: 24 } },
+          delta: { reference: 10, increasing: { color: "RebeccaPurple" } },
+          gauge: {
+            axis: { range: [null, 10], tickwidth: 10, tickcolor: "darkblue" },
+            bar: { color: "darkblue" },
+            bgcolor: "black",
+            borderwidth: 2,
+            bordercolor: "lightblue",
+            steps: [
+              { range: [0, 1], color: "rgb(111,600,399)" },
+              { range: [1, 2], color: "rgb(000,400,399)" },
+              { range: [2, 3], color: "rgb(000,400,399)" },
+              { range: [3, 4], color: "rgb(000,400,499)" },
+              { range: [4, 5], color: "rgb(000,400,499)" },
+              { range: [5, 6], color: "rgb(000,400,499)" },
+              { range: [6, 7], color: "rgb(000,400,499)" },
+              { range: [7, 8], color: "rgb(000,400,499)" },
+              { range: [8, 9], color: "rgb(000,400,499)" },
+              { range: [9, 10], color: "rgb(000,400,599)" }
+            ],
+            threshold: {
+              line: { color: "red", width: 4 },
+              thickness: 0.75,
+              value: 490
+            }
+          }
+        }
+      ];
+      
+      var layout = {
+        width: 500,
+        height: 400,
+        margin: { t: 25, r: 25, l: 25, b: 25 },
+        font: { color: "darkblue", family: "Arial" }
+      };
+      
+      Plotly.newPlot('gauge', data, layout);
+    })
+  };
+    
+function init() {
+  var dropdown = d3.select("#selDataset");
+  
+  d3.json("../samples.json").then((selectedData) => {
+      selectedData.names.forEach(function(name) {
+          dropdown.append("option").text(name).property("value");
+      });
+      buildGaugeChart(selectedData.names[0]);
+  });
+};
+
+init();
